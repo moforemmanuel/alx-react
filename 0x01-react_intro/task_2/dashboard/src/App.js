06f92b4905786be552b387a52dca78ad17eb63dd
= <=>
2022-05-07 00:56:08 +0100

    task_2

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <>
+    <div className='App-header'>
+      <img src={logo} alt='Holberton Logo' />
+      <h1>School Dashboard</h1>
+    </div>
+
+    <div className='App-body'>
+      <p>Login to access the full dashboard</p>
+      <div className='form-control'>
+        <label for='email'>Email</label>
+        <input type='email' name='email' id='email' />
+      </div>
+      <div className='form-control'>
+        <label for='password'>Password</label>
+        <input type='password' name='password' id='password' />
+      </div>
+      <button type='submit'>OK</button>
+    </div>
+
+    <div className='App-footer'>
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+    </div>
+
+    </>
+  );
+  
+}
+
+
+export default App;
