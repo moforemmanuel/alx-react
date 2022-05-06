2dad9eb3b9e23821ad6e97f7a48d88e09548de0a
= <=>
2022-05-07 00:36:31 +0100

    task_1

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -9,9 +9,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Notifications />
+    <div className='root-notificications'>
+      <Notifications />
+    </div>
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
