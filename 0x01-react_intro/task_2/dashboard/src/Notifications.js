e06aa593b7098141f548b6fe289aa80f6d172083
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-05-25 20:53:00 +0100

    fixed styles

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -7,6 +7,11 @@ export default function Notifications() {
     <>
     <div className='Notifications'>
       <p>Here is the list of notifications</p>
+      <ul>
+      <li className='text-navy'>New course available</li>
+      <li className='text-red'>New resume available</li>
+      <li className='text-red'><b>Urgent requirement</b> - complete by EOD</li>
+      </ul>
     </div>
 
     <button className='closeBtn' aria-label='Close' onClick={() => console.log('Close button has been clicked')}>
