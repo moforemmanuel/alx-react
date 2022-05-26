edd149feb64855ca7260a218cdb32743061b219e
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-05-26 21:39:37 +0100

    map

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,6 +1,4 @@
-// import Immutable from "./node_modules/immutable/dist/immutable";
 import { fromJS, Map } from "immutable";
-
 export default function getImmutableObject(object) {
   return Map(object);
 };
