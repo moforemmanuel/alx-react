f06c0f1b8f24dd7858a1efab915fa72d7b293432
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-05-27 04:57:54 +0100

    nested

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
 import { fromJS } from './node_modules/immutable/dist/immutable';
 
 export default function accessImmutableObject(object, array) {
-  return fromJS(object).getIn(array);
+  return Map(object).getIn(array);
 }
